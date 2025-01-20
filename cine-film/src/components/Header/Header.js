f553a729cfb5387d7e182764dab0f0ab33c86946
Rafael Debein <debeinrafael@gmail.com>
2025-01-20 15:55:13 +0100

    Ajout du Header et de son style

--- cine-film/src/components/Header/Header.js
+++ cine-film/src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import logo from "../../assets/images/logo.png";
 import styles from "./Header.module.css";
@@ -6,13 +7,23 @@ import styles from "./Header.module.css";
 const Header = () => {
 	return (
 		<header className={styles.header}>
-			<div className={styles.logoContainer}>
-				<img src={logo} alt="Cine Film" className={styles.headerLogo} />
-				<h1 className={styles.headerTitle}>Cine Film</h1>
+			<div className={styles.logo}>
+				<img src={logo} alt="CineFilm" className={styles.logoImage} />
+				<h1 className={styles.logoText}>CineFilm</h1>
 			</div>
-			<h1 className={styles.headerTitle}>Accueil</h1>
-			<h1 className={styles.headerTitle}>"Série"</h1>
-			<h1 className={styles.headerTitle}>"Films"</h1>
+			<nav className={styles.nav}>
+				<ul className={styles.navList}>
+					<li className={styles.navItem}>
+						<NavLink to="/" className={({ isActive }) => (isActive ? styles.active : styles.navLink)}>Accueil</NavLink>
+					</li>
+					<li className={styles.navItem}>
+						<NavLink to="/movies" className={({ isActive }) => (isActive ? styles.active : styles.navLink)}>"Films"</NavLink>
+					</li>
+					<li className={styles.navItem}>
+						<NavLink to="/series" className={({ isActive }) => (isActive ? styles.active : styles.navLink)}>"Séries"</NavLink>
+					</li>
+				</ul>
+			</nav>
 		</header>
 	);
 };
--- cine-film/src/index.js
+++ cine-film/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 
-// import './styles/index.css'
 import Home from './pages/Home/Home'
+import Movie from './pages/Movies/Movies'
+import Series from './pages/Series/Series'
+import Header from './components/Header/Header'
+
+import './styles/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<Home />
+		<Router>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/movies" element={<Movie />} />
+				<Route path="/series" element={<Series />} />
+			</Routes>
+		</Router>
 	</React.StrictMode>
 );
